308ad1b6364e8c1522f11267df6c88f798208332
Cozart98 <chihaoui1098@gmail.com>
2017-09-06 11:14:35 +0200

    ajout fichier android studio

---
+++ app/src/main/java/com/example/apprenti/wiki_monster_legends/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.apprenti.wiki_monster_legends;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
